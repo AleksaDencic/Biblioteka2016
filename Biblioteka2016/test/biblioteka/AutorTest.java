2e045519d9a5f1e6723258e0f9e518af7cbc4ec5
AleksaDencic <alekdencic@hotmail.com>
2016-03-23 18:56:52 +0100

    Biblioteka JUnit 4.0 dodata u classpath i napravljen test klase Autor.

---
+++ Biblioteka2016/test/biblioteka/AutorTest.java
@@ -0,0 +1,81 @@
+package biblioteka;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AutorTest {
+
+	private Autor a;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		a = new Autor();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		a = null;
+	}
+
+	
+	@Test
+	public void testSetImeSveOk() {
+		String ime = "Pera";
+		
+		a.setIme(ime);
+		
+		assertEquals(ime, a.getIme());
+	}
+	
+	@Test
+	public void testSetImeSveOk2() {
+		String ime = "Al";
+		
+		a.setIme(ime);
+		
+		assertEquals(ime, a.getIme());
+	}
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testSetImeNull() {
+		a.setIme(null);
+	}
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testSetImePrazan() {
+		a.setIme("");
+	}
+	
+	@Test
+	public void testSetPrezimeSveOk() {
+		String prezime = "Pera";
+		
+		a.setPrezime(prezime);
+		
+		assertEquals(prezime, a.getPrezime());
+	}
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testSetPrezimeNull() {
+		a.setPrezime(null);
+	}
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testSetPrezimePrazan() {
+		a.setPrezime("");
+	}
+
+}
